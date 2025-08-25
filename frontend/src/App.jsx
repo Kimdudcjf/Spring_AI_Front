a7609740cd9cdca392b7bbf98fd1248ed14263e8
Kimdudcjf <dudcjf0113@naver.com>
2025-08-26 00:33:48 +0900

    JoinPage, 라우터

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import JoinPage from "./pages/JoinPage";
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/join" element={<JoinPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
